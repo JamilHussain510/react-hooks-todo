4fd4bc0a6a38a8a7bff71511e0d12d0370299ff2
JamilHussain572 <50754301+JamilHussain572@users.noreply.github.com>
2020-06-27 22:04:13 +0400

    Updates

---
+++ precache-manifest.19e5bfb064301b6674d3ded9ee3a510a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b764628f68e0192ae1c5181e4671684a",
+    "url": "/react-hooks-todo/index.html"
+  },
+  {
+    "revision": "e67dd4a11986c43b3e34",
+    "url": "/react-hooks-todo/static/css/main.035bd154.chunk.css"
+  },
+  {
+    "revision": "cd6964dc5c66daff2bf8",
+    "url": "/react-hooks-todo/static/js/2.a35f3e16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-todo/static/js/2.a35f3e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67dd4a11986c43b3e34",
+    "url": "/react-hooks-todo/static/js/main.3768f1f1.chunk.js"
+  },
+  {
+    "revision": "0846bed1e7accc260f3c",
+    "url": "/react-hooks-todo/static/js/runtime-main.aa7ec90f.js"
+  }
+]);
